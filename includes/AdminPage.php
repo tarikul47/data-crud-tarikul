a427129e1f13128667b18cbe8aad1195acf70023
Tarikul Islam <34490496+tarikul47@users.noreply.github.com>
2024-04-17 05:11:16 +0600

    first commit

--- includes/AdminPage.php
+++ includes/AdminPage.php
@@ -21,7 +21,8 @@ class AdminPage
      */
     function add_admin_menu()
     {
-        add_menu_page('DB Crud', 'DB Crud', 'manage_options', 'db-crud?tab=list', [$this, 'admin_page'], 'dashicons-admin-generic', 25);
+        add_menu_page('DB Crud', 'DB Crud', 'manage_options', 'db-crud', [$this, 'admin_page'], 'dashicons-admin-generic', 25);
+        add_submenu_page('db-crud', 'DB Crud List', 'List', 'manage_options', 'db-crud&tab=list', [$this, 'admin_page']);
     }
 
     /**
@@ -38,11 +39,13 @@ class AdminPage
             <ul class="flex flex-wrap border-b border-gray-200 dark:border-gray-700">
                 <li class="mr-2 bg-gray-100">
                     <a href="#"
-                        class="inline-block text-blue-600 hover:text-blue-700 hover:bg-gray-50 rounded-t-lg py-4 px-4 text-sm font-medium text-center">Data List</a>
+                        class="inline-block text-blue-600 hover:text-blue-700 hover:bg-gray-50 rounded-t-lg py-4 px-4 text-sm font-medium text-center">Data
+                        List</a>
                 </li>
                 <li class="mr-2 inactive">
                     <a href="#"
-                        class="inline-block text-blue-600  hover:text-blue-700 hover:bg-gray-50 rounded-t-lg py-4 px-4 text-sm font-medium text-center">Add New Data</a>
+                        class="inline-block text-blue-600  hover:text-blue-700 hover:bg-gray-50 rounded-t-lg py-4 px-4 text-sm font-medium text-center">Add
+                        New Data</a>
                 </li>
             </ul>
             <div class="px-1 py-2">
