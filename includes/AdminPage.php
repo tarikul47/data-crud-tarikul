d060fad7b2f159f656f81e6b1d3811f21faad304
Tarikul Islam <34490496+tarikul47@users.noreply.github.com>
2024-04-17 16:54:54 +0600

    first commit

--- includes/AdminPage.php
+++ includes/AdminPage.php
@@ -22,7 +22,8 @@ class AdminPage
     function add_admin_menu()
     {
         add_menu_page('DB Crud', 'DB Crud', 'manage_options', 'db-crud', [$this, 'admin_page'], 'dashicons-admin-generic', 25);
-        add_submenu_page('db-crud', 'DB Crud List', 'List', 'manage_options', 'db-crud&tab=list', [$this, 'admin_page']);
+        add_submenu_page('db-crud', 'DB Crud List', 'Data List', 'manage_options', 'db-crud', [$this, 'admin_page']);
+        add_submenu_page('db-crud', 'Add New Data', 'Add New Data', 'manage_options', 'db-crud&tab=add', [$this, 'admin_page']);
     }
 
     /**
@@ -32,27 +33,55 @@ class AdminPage
     function admin_page()
     {
         $active_tab = isset($_GET['tab']) ? $_GET['tab'] : 'list';
-        var_dump($active_tab);
+        var_dump(get_admin_url() . 'admin.php');
+        var_dump(add_query_arg('tab', 'list', get_admin_url('admin')));
         ?>
         <!-- Tabs -->
         <div class="mx-6 mt-6">
             <ul class="flex flex-wrap border-b border-gray-200 dark:border-gray-700">
-                <li class="mr-2 bg-gray-100">
-                    <a href="#"
+                <li class="mr-2 <?php echo $active_tab === 'list' ? ' bg-gray-100' : '' ?>">
+                    <a href="
+                    <?php
+                    echo add_query_arg(
+                        [
+                            'page' => 'db-crud',
+                            'tab' => 'list'
+                        ],
+                        get_admin_url() . 'admin.php'
+                    ) ?>
+                    "
                         class="inline-block text-blue-600 hover:text-blue-700 hover:bg-gray-50 rounded-t-lg py-4 px-4 text-sm font-medium text-center">Data
                         List</a>
                 </li>
-                <li class="mr-2 inactive">
-                    <a href="#"
+                <li class="mr-2 <?php echo $active_tab === 'add' ? ' bg-gray-100' : '' ?>">
+                    <a href="
+                    <?php
+                    echo add_query_arg(
+                        [
+                            'page' => 'db-crud',
+                            'tab' => 'add'
+                        ],
+                        get_admin_url() . 'admin.php'
+                    ) ?>
+                    "
                         class="inline-block text-blue-600  hover:text-blue-700 hover:bg-gray-50 rounded-t-lg py-4 px-4 text-sm font-medium text-center">Add
                         New Data</a>
                 </li>
             </ul>
             <div class="px-1 py-2">
-                <h3 class="text-lg font-semibold">Data List</h3>
-            </div>
-            <div class="hidden px-1 py-2">
-                <h3 class="text-lg font-semibold">Add New Data</h3>
+                <?php
+                switch ($active_tab) {
+                    case 'add':
+                        # code...
+                        echo "Add";
+                        break;
+
+                    default:
+                        # code...
+                        echo "List";
+                        break;
+                }
+                ?>
             </div>
         </div>
         <?php
